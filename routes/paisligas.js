7b0725e8378bbd1d5c706a9c6aae4ce67020db4b
Jesus Diaz <jesus_ad019@hotmail.com>
2021-05-25 19:10:27 -0500

    nueva version

--- routes/paisligas.js
+++ routes/paisligas.js
@@ -51,6 +51,7 @@ router.post('/save', async (req, res) => {
         }else if(api_res.body.response != 0){
             var data = api_res.body.response;
             var list = [];
+            var message = [];
             var today = new Date();
             var dd = String(today.getDate()).padStart(2, '0');
             var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -69,28 +70,28 @@ router.post('/save', async (req, res) => {
                     });   
                 }            
             }
-                try {
-                    const paisliga = new Paisliga({
-                        pais: data[0].country.name,
-                        ligas: list,
-                    });
-                    const result = Paisliga.updateOne({
-                        pais: data[0].country.name
-                    },
-                    {
-                        ligas: paisliga.ligas
-                    });
-                    if(result.n == 0){
-                        const save = paisliga.save();
-                        res.json({"message": "SE HA GUARDADO CON ÉXITO", "value": 202});
-                    }else if(result.nModified == 0){
-                        res.json({"message": "NO HUBO CAMBIOS AL MODIFICAR", "value": 200});
-                    }else{
-                        res.json({"message": "SE HA MODIFICADO CON ÉXITO", "value": 204});
-                    }
-                } catch (error) {
-                    console.log(error+"xd");
+            try {
+                const paisliga = new Paisliga({
+                    pais: data[0].country.name,
+                    ligas: list,
+                });
+                const result = Paisliga.updateOne({
+                    pais: data[0].country.name
+                },
+                {
+                    ligas: paisliga.ligas
+                });
+                if(result.n == 0){
+                    const save = paisliga.save();
+                    res.json({"message": "SE HA GUARDADO CON ÉXITO", "value": 202});
+                }else if(result.nModified == 0){
+                    res.json({"message": "NO HUBO CAMBIOS AL MODIFICAR", "value": 200});
+                }else{
+                    res.json({"message": "SE HA MODIFICADO CON ÉXITO", "value": 204});
                 }
+            } catch (error) {
+                console.log(error+"xd");
+            }
         }else{
             res.json({"message": "NO HAY LIGAS ACTIVAS DE "+pais, "value": 300});
         }
